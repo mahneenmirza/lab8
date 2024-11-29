5550d57e376f2203592a929031174e34a21ab916
mahneenmirza <233555@students.au.edu.pk>
2024-11-29 15:38:06 +0500

    Add files via upload

---
+++ Activity_9/Form3.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity_9
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            
+        }
+
+        private void customerEntryToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form1 obi = new Form1();
+            obi.MdiParent = this;
+            obi.Show();
+        }
+    }
+}
